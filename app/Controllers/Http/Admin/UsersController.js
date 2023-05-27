4a8f60e7fc6ba1fb8f1813b03740b8098d513a80
WaiYanLinn <waiyanlinn.lion@outlook.com>
2023-05-27 18:58:19 +0630

    'deploy-test'

---
+++ app/Controllers/Http/Admin/UsersController.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class UsersController {
+    index({ inertia }) {
+        return inertia.render('Admin/Users/UserLists');
+    }
+}
+exports.default = UsersController;
+//# sourceMappingURL=UsersController.js.map
---
+++ prisma/dummy.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const dummy = {
+    cinemas: [
+        "Adonis Ace Cineplex",
+        "Adonis Prisma Cineplex",
+        "Inertia Ace Cineplex",
+        "Vue Ace Cineplex",
+    ],
+    halls: {
+        "Aonis Ace Cineplex": ["Hall 1", "Hall 2", "Hall 3", "Hall 4"],
+        "Peisma Ace Cineplex": ["Hall 1", "Hall 2", "Hall 3", "Hall 4"],
+        "Inertia Ace Cineplex": ["Hall 1", "Hall 2", "Hall 3", "Hall 4"],
+        "Vue Ace Cineplex": ["Hall 1", "Hall 2", "Hall 3", "Hall 4"],
+    },
+};
+exports.default = dummy;
+//# sourceMappingURL=dummy.js.map
